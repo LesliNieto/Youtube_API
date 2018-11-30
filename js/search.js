b7a4749c6f2baccdad804bf751e7123463471739
Lesli Nieto <lesli.nieto@koombea.com>
2018-11-30 15:49:20 -0500

    Videos already play

---
+++ js/search.js
@@ -0,0 +1,24 @@
+function search(){
+  var searchInput = $("#query").val();
+  $.getJSON ('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+searchInput
+    +'&maxResults=15&key=AIzaSyDXPy1WWSZ1Sk55U3w3nr8-D64cF8otsag',
+    function(response){
+    var results = response.items
+    $.each(results, function(key, element){
+      if(element.id.kind == "youtube#video"){
+        $("#video").append(
+          '<iframe id="player"  src=https://www.youtube.com/embed/'
+          +element.id.videoId
+          +'></iframe><br>'
+          +'<h4 >'+element.snippet.title+'</h4>'
+          +'<pre> Channel: '+element.snippet.channelTitle+'</pre>'
+        )
+      };
+    });
+  });
+ $("#video").html("");
+};
+
+
+
+
